7aba5fc51f406856a2d56c81db736797f7b17f02
Shane Sveller <shanesveller@gmail.com>
2011-03-30 19:56:59 +0000

    Initial import
    git-svn-id: https://df2010backup.svn.codeplex.com/svn@4513 0ce29038-4a70-45ae-8a1e-7d2f2ce429ee

---
+++ DFBackupAssistant/DFBackupAssistant/App.xaml.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Linq;
+using System.Windows;
+
+namespace DFBackupAssistant
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+    }
+}
