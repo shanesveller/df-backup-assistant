3dc5920f5998fed53b46fa37f88d0b3cad1e6df3
Shane Sveller <shanesveller@gmail.com>
2011-04-01 16:10:50 +0000

    Add Classes for Backups
    git-svn-id: https://df2010backup.svn.codeplex.com/svn@4641 0ce29038-4a70-45ae-8a1e-7d2f2ce429ee

---
+++ DFBackupAssistant/DFBackupAssistant/Backup.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.IO;
+using System.Text;
+using Ionic.Zip;
+
+namespace DFBackupAssistant
+{
+    public class Backup
+    {
+        public string Filename { get; set; }
+        public string Name { get; set; }
+        public Backup(string f, string n)
+        {
+            this.Filename = f;
+            this.Name = n;
+        }
+        public bool Restore(string folder, string name, bool overwrite = false)
+        {
+            using (ZipFile zip = new ZipFile(this.Filename))
+            {
+                if (overwrite)
+                    zip.ExtractExistingFile = ExtractExistingFileAction.OverwriteSilently;
+                else
+                {
+                    DirectoryInfo di = new DirectoryInfo(Path.Combine(folder, name));
+                    if (di.Exists)
+                    {
+                        return false;
+                    }
+                }
+                zip.ExtractAll(folder);
+                return true;
+            }
+        }
+    }
+}
---
+++ DFBackupAssistant/DFBackupAssistant/BackupDirectory.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+
+namespace DFBackupAssistant
+{
+    class BackupDirectory
+    {
+        public string FullPath { get; set; }
+        public List<Backup> Backups { get; set; }
+
+        BackupDirectory(string f)
+        {
+            this.FullPath = f;
+            this.Backups = new List<Backup>();
+            FileInfo[] backupFiles = new DirectoryInfo(f).GetFiles("*.zip");
+            foreach (FileInfo file in backupFiles)
+                this.Backups.Add(new Backup(file.FullName, file.Name));
+        }
+    }
+}
