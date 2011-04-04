e0637de1ea8baeb2cce5d05290c6cfef91d99896
Shane Sveller <shanesveller@gmail.com>
2011-04-04 20:42:55 +0000

    Add example directory watchers to SaveDirectory
    git-svn-id: https://df2010backup.svn.codeplex.com/svn@4814 0ce29038-4a70-45ae-8a1e-7d2f2ce429ee

--- DFBackupAssistant/DFBackupAssistant/Classes/SaveDirectory.cs
+++ DFBackupAssistant/DFBackupAssistant/Classes/SaveDirectory.cs
@@ -11,12 +11,24 @@ namespace DFBackupAssistant
         public string FullPath { get; set; }
         public List<Save> SaveGames { get; set; }
         public DirectoryInfo DirectoryInfo { get { return new DirectoryInfo(this.FullPath); } }
+        private FileSystemWatcher Watcher { get; set; }
+        public bool Watching {
+            get { return this.Watcher.EnableRaisingEvents; }
+            set { this.Watcher.EnableRaisingEvents = value; }
+        }
 
         public SaveDirectory(string fp)
         {
             this.FullPath = fp;
             this.SaveGames = new List<Save>();
             this.RefreshSaveGames();
+
+            this.Watcher = new FileSystemWatcher(fp, "world.sav");
+            this.Watcher.IncludeSubdirectories = true;
+            this.Watcher.Changed += this.watcherChanged;
+            this.Watcher.Created += this.watcherCreated;
+            this.Watcher.Deleted += this.watcherDeleted;
+            this.Watcher.EnableRaisingEvents = true;
         }
 
         public void RefreshSaveGames()
@@ -25,6 +37,24 @@ namespace DFBackupAssistant
             DirectoryInfo[] dirs = new DirectoryInfo(this.FullPath).GetDirectories();
             foreach (DirectoryInfo subDir in dirs)
                 this.SaveGames.Add(new Save(this.FullPath, subDir.Name));
+        }
+
+        private void watcherChanged(object sender, System.IO.FileSystemEventArgs e)
+        {
+            System.Windows.MessageBox.Show(e.ChangeType + ": " + e.FullPath);
+            this.RefreshSaveGames();
+        }
+
+        private void watcherCreated(object sender, System.IO.FileSystemEventArgs e)
+        {
+            System.Windows.MessageBox.Show(e.ChangeType + ": " + e.FullPath);
+            this.RefreshSaveGames();
+        }
+
+        private void watcherDeleted(object sender, System.IO.FileSystemEventArgs e)
+        {
+            System.Windows.MessageBox.Show(e.ChangeType + ": " + e.FullPath);
+            this.RefreshSaveGames();
         }
     }
 }
