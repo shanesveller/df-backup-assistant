0539225ef707f109d4faa736c76a6709502351df
Shane Sveller <shanesveller@gmail.com>
2011-04-05 15:37:59 +0000

    Incomplete DFSettings class
    git-svn-id: https://df2010backup.svn.codeplex.com/svn@4891 0ce29038-4a70-45ae-8a1e-7d2f2ce429ee

---
+++ DFBackupAssistant/DFBackupAssistant/Classes/DFSettings.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+
+namespace DFBackupAssistant.Classes
+{
+    public class DFSettings
+    {
+        public enum AutoSaveType { None, Yearly, Seasonal };
+        
+        public AutoSaveType SaveType { get; set; }
+        public bool AutoBackup { get; set; }
+        public bool CompressSaves { get; set; }
+        public bool InitialSave { get; set; }
+        public bool PauseOnAutosave { get; set; }
+        public bool PauseOnLoad { get; set; }
+
+        private FileInfo InitTxt { get; set; }
+        private FileInfo DInitTxt { get; set; }
+
+        public DFSettings(string path)
+        {
+            FileInfo exe = new FileInfo(path);
+            this.InitTxt = new FileInfo(Path.Combine(exe.DirectoryName, "data", "init.txt"));
+            this.DInitTxt = new FileInfo(Path.Combine(exe.DirectoryName, "data", "d_init.txt"));
+            if (!InitTxt.Exists || DInitTxt.Exists)
+                throw new NullReferenceException("init.txt or d_init.txt missing.");
+        }
+    }
+}
